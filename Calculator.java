eb44b1a0ef087de9b65ad0f54138a7c2253e93a7
pavelegorovkrsn <egorovP9009@yandex.ru>
2026-06-05 11:04:28 +0400

    feat: add multiplyV2

--- Calculator.java
+++ Calculator.java
@@ -2,4 +2,6 @@ public class Calculator{
   public int add(int a, int b){ return a+b;}
   
   public int subtract(int a, int b){ return a-b; }
+  
+  public int myltiplyV2(int a, int b) { return a * b;}
 }

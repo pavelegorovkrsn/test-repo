53eca50a4e1ae4f60a598d602552a40ebb40c158
pavelegorovkrsn <egorovP9009@yandex.ru>
2026-06-07 17:05:05 +0400

    feat: add divide method

--- Calculator.java
+++ Calculator.java
@@ -6,5 +6,7 @@ public class Calculator{
   public int myltiplyV1(int a, int b){ return a * b; }
 
   public int myltiplyV2(int a, int b) { return a * b;}
+  
+  public int divide (int a, int b) { return a/b; }
 
 }
